88eab3156659084486269bd668b67fbe74ddb810
hayakawasho <hayakawa@spreadsytems.co.jp>
2016-02-16 19:48:44 +0900

    Add:ESLintの導入

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,20 @@ var webpackConfig = {
       //jsonpFunction: 'fr'
    },
    resolve: {
-      extensions: ['', '.js', '.coffee'], //拡張子を省略できる
+      //extensions: ['', '.js', '.coffee'], //拡張子を省略できる
       modulesDirectories: [
-         //'bower_components',
+         'bower_components',
          'node_modules',
+         'src'
       ],
       alias: {}
    },
    module: {
+      preLoaders: [{
+         test: /\.js$/,
+         exclude: /Spec\.js$/i,
+         loaders: ['eslint']
+      }],
       loaders: [{
          test: /\.(js)$/,
          exclude: /node_modules/,
@@ -23,10 +29,20 @@ var webpackConfig = {
       }]
    },
    plugins: [
-      //new webpack.optimize.UglifyJsPlugin({}),
+      new webpack.optimize.UglifyJsPlugin({
+         exclude: /Spec\.js$/i,
+         compress: {
+            warnings: false
+         }
+      }),
       new webpack.optimize.AggressiveMergingPlugin(), //ファイルを細かく分析し、まとめられるところはできるだけまとめてコードを圧縮する
       new webpack.optimize.DedupePlugin(), // ライブラリ間で依存しているモジュールが重複している場合、二重に読み込まないようにする
-   ]
+      new webpack.optimize.OccurenceOrderPlugin(),
+   ],
+   eslint: {
+      configFile: '.eslintrc',
+      failOnError: true
+   }
 };
 
 module.exports = webpackConfig;
